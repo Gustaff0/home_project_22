a8d4b39b93420b3a929cdb267c6a17de1afb66a4
Bars <barsbekclash@gmail.com>
2021-02-22 10:06:20 +0600

    refresh urls.py

--- play/play/urls.py
+++ play/play/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import action_play
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', action_play)
 ]
